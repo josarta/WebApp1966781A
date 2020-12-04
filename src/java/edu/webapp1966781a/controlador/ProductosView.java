545864fbd750466944a3df4a269323540b69943d
Usuario <Usuario@DESKTOP-99ROSGA>
2020-12-03 20:34:55 -0500

    Carga de inicial de archivos

--- src/java/edu/webapp1966781a/controlador/ProductosView.java
+++ src/java/edu/webapp1966781a/controlador/ProductosView.java
@@ -10,15 +10,32 @@ import edu.webapp1966781a.entity.Producto;
 import edu.webapp1966781a.entity.Usuario;
 import edu.webapp1966781a.facade.CategoriaFacadeLocal;
 import edu.webapp1966781a.facade.ProductoFacadeLocal;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import javax.servlet.http.Part;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.primefaces.PrimeFaces;
+import org.primefaces.event.FileUploadEvent;
 
 /**
  *
@@ -35,12 +52,12 @@ public class ProductosView implements Serializable {
 
     private Categoria objCategoria = new Categoria();
     private ArrayList<Categoria> listaCategorias = new ArrayList<>();
-    
+
     private Producto objProductoNew = new Producto();
     private int id_cat_selec = 0;
-    
-    
-    
+    private Part archivoImagen;
+    private Part archivoExcel;
+    private String nombreArchivo;
 
     /**
      * Creates a new instance of ProductosView
@@ -66,21 +83,144 @@ public class ProductosView implements Serializable {
         return productoFacadeLocal.listaPorCategoria(objCategoria.getIdcategorias());
     }
 
+    public void subeImagen() {
+        String mensajeSw = "";
+        if (archivoImagen != null) {
+            if (archivoImagen.getSize() < 4000000) {
+                if ("image/jpeg".equals(archivoImagen.getContentType())) {
+                    File carpeta = new File("C:/imgdashio/Productos/Categorias");
+                    if (!carpeta.exists()) {
+                        carpeta.mkdirs();
+                    }
+                    try (InputStream is = archivoImagen.getInputStream()) {
+                        SimpleDateFormat ffecha = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+                        Calendar hoy = Calendar.getInstance();
+                        String renombraArchivo = ffecha.format(hoy.getTime()) + ".";
+                        renombraArchivo += FilenameUtils.getExtension(archivoImagen.getSubmittedFileName());
+                        Files.copy(is, (new File(carpeta, renombraArchivo)).toPath(), StandardCopyOption.REPLACE_EXISTING);
+                        nombreArchivo = renombraArchivo;
+                    } catch (Exception e) {
+                    }
+
+                } else {
+                    mensajeSw = "swal('El archivo' , ' no es una imagen ', 'error')";
+                }
+            } else {
+                mensajeSw = "swal('La imagen' , ' es muy grande  ', 'error')";
+            }
+        } else {
+            mensajeSw = "swal('No se subio' , ' Una imagen  ', 'error')";
+        }
+
+        PrimeFaces.current().executeScript(mensajeSw);
+    }
+
+    public void insertarXLS(List cellDataList) {
+        try {
+            int filasContador = 0;
+            for (int i = 0; i < cellDataList.size(); i++) {
+                List cellTemp = (List) cellDataList.get(i);
+                Producto newP = new Producto();
+                for (int j = 0; j < cellTemp.size(); j++) {
+                    XSSFCell hssfCell = (XSSFCell) cellTemp.get(j);
+                    switch (filasContador) {
+                        case 0:
+                            newP.setSerial(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 1:
+                            newP.setNombre(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 2:
+                            newP.setImagenRuta(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 3:
+                            newP.setCantidad(hssfCell.toString());
+                            filasContador++;
+                            break;
+                        case 4:
+                            newP.setValorCompra(hssfCell.getNumericCellValue());
+                            filasContador++;
+                            break;
+                        case 5:
+                            newP.setValorVenta(hssfCell.getNumericCellValue());
+                            filasContador++;
+                            break;
+                        case 6:
+                            Categoria nueva = categoriaFacadeLocal.find((int) Math.floor(hssfCell.getNumericCellValue()));
+                            newP.setFkCategoria(nueva);
+                            productoFacadeLocal.create(newP);
+                            filasContador = 0;
+                            break;
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+        }
+    }
+
+    public void subeExcel() throws IOException {
+        String mensajeSw = "";
+        if (archivoExcel != null) {
+            if (archivoExcel.getSize() < 4000000) {
+                if ("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet".equals(archivoExcel.getContentType())) {
+                    InputStream input = archivoExcel.getInputStream();
+                    List cellData = new ArrayList();
+                    try {
+                        XSSFWorkbook workBook = new XSSFWorkbook(input);
+                        XSSFSheet hssfSheet = workBook.getSheetAt(0);
+                        Iterator rowIterator = hssfSheet.rowIterator();
+                        rowIterator.next();
+
+                        while (rowIterator.hasNext()) {
+                            XSSFRow hssfRow = (XSSFRow) rowIterator.next();
+                            Iterator iterator = hssfRow.cellIterator();
+                            List cellTemp = new ArrayList();
+                            while (iterator.hasNext()) {
+                                XSSFCell hssfCell = (XSSFCell) iterator.next();
+                                cellTemp.add(hssfCell);
+                            }
+                            cellData.add(cellTemp);
+                        }
+                        insertarXLS(cellData);
+                    } catch (Exception e) {
+                        PrimeFaces.current().executeScript("swal('Problemas ingresando el archivo' , 'error');");
+                    }
+                    ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+                    context.redirect("index.xhtml");
+                } else {
+                    mensajeSw = "swal('El archivo' , ' no es una XLSX ', 'error')";
+                }
+            } else {
+                mensajeSw = "swal('El archivo' , ' es muy grande  ', 'error')";
+            }
+        } else {
+            mensajeSw = "swal('No puede cargar ' , ' EL  archivo  ', 'error')";
+        }
+
+        PrimeFaces.current().executeScript(mensajeSw);
+    }
+
     public void crearProducto() {
         String mensajeSw = "";
         try {
             Categoria obt = categoriaFacadeLocal.find(id_cat_selec);
             objProductoNew.setFkCategoria(obt);
+            objProductoNew.setImagenRuta(nombreArchivo);
             productoFacadeLocal.create(objProductoNew);
             objProductoNew = new Producto();
-            mensajeSw = "swal('Usuario registrado' , ' con exito ', 'success')";
+            mensajeSw = "swal('Producto registrado' , ' con exito ', 'success')";
         } catch (Exception e) {
-            mensajeSw = "swal('El usuario' , ' Ya se encuentra registrado  ', 'error')";
+            mensajeSw = "swal('Problemas ingresando ' un nuevo producto  ', 'error')";
         }
-      
+
         PrimeFaces.current().executeScript(mensajeSw);
     }
-    
+
     public int catidadProductos() {
         return productoFacadeLocal.count();
     }
@@ -89,9 +229,6 @@ public class ProductosView implements Serializable {
         objCategoria = categoriaFacadeLocal.find(idCategoria);
     }
 
-    
-    
-    
     public void setObjCategoria(Categoria objCategoria) {
         this.objCategoria = objCategoria;
     }
@@ -122,6 +259,30 @@ public class ProductosView implements Serializable {
 
     public void setId_cat_selec(int id_cat_selec) {
         this.id_cat_selec = id_cat_selec;
+    }
+
+    public Part getArchivoImagen() {
+        return archivoImagen;
+    }
+
+    public void setArchivoImagen(Part archivoImagen) {
+        this.archivoImagen = archivoImagen;
+    }
+
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    public Part getArchivoExcel() {
+        return archivoExcel;
+    }
+
+    public void setArchivoExcel(Part archivoExcel) {
+        this.archivoExcel = archivoExcel;
     }
 
 }
---
+++ src/java/edu/webapp1966781a/utilidades/ServletImg.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.webapp1966781a.utilidades;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Usuario
+ */
+@WebServlet("/images/*")
+public class ServletImg extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletImg</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletImg at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String filename = request.getPathInfo().substring(1);
+        if (!filename.isEmpty()) {
+            String[] ruta = filename.split("/");
+            File folder = new File("C:\\imgdashio\\Productos\\Categorias\\" + filename);
+            System.out.println(folder.getAbsolutePath());
+
+            //     File file = new File(rutaCarpeta+"/"+ruta[0] , ruta[1]);
+            response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+            response.setHeader("Content-Length", String.valueOf(folder.length()));
+            response.setHeader("Content-Disposition", "inline; filename=\"" + filename + "\"");
+            try {
+                Files.copy(folder.toPath(), response.getOutputStream());
+                processRequest(request, response);
+            } catch (IOException | ServletException e) {
+                System.out.println("--> " + e.getMessage());
+
+            }
+        }
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
